0ea5cd4767b7faf4371118e963d9028eb1149c16
macintosh <macintosh@obama.local>
2012-12-31 20:38:34 +0400

    first commit

--- example.rb
+++
@@ -1,5 +0,0 @@
-HandleSearch::Base.define_base(Face)
-HandleSearch::Base.define_attribute(:fog, HandleSearch::DateWrapper)
-HandleSearch::Base.define_attribute(:gof)
-HandleSearch::Base.define_association(:residences)
-h = HandleSearch::Base.new
